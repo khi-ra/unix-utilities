62712f70f6683af0b0b542a1ea4aabfeab8a2516
Khizer Rafiq <mekhizer@gmail.com>
2026-05-28 03:33:19 +0100

    started implementing 'file_exists()', laid out general idea

--- cat/cat.c
+++ cat/cat.c
@@ -1,12 +1,10 @@
-#include <linux/limits.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
-//#include <errno.h>
 #define MAXINPUT 1024
-ssize_t get_input(char *input_buf);
+ssize_t read_input(char *input_buf);
 
-ssize_t get_input(char *input_buf) {
+ssize_t read_input(char *input_buf) {
   ssize_t n = -1;
 
 
@@ -16,18 +14,39 @@ ssize_t get_input(char *input_buf) {
     return n;
   }
 
-  // null terminate buffer
+  // strip trailing '\n' and null terminate buffer
   input_buf[n] = 0;
   input_buf[strcspn(input_buf, "\n")] = 0;
   return n;
 }
 
-int main() {
+int file_exists(char *input_buf) {
+  /* takes input and checks if arg file eists */
+  /* if path exists with `/` its absolute */
+
+  /* i could just use opendir() instead of manually handling this
+   * look into that and see what i need to do. i might also just
+   * make my own version of 'opendir', ill read through what it does and see
+   * if that seems reasonable.
+   */
+  int is_absolute_path = 0;
+  int file_exists = 0;
+
+  if (input_buf[0] == '/') {
+    is_absolute_path = 1;
+  }
+
+  if (is_absolute_path) {
+
+  }
+}
+
+int main(int argc, char **argv) {
   // +1 for null termination character
   char input_buf[MAXINPUT+1];
   ssize_t n;
 
-  while ((n = get_input(input_buf)) > 0) {
+  while ((n = read_input(input_buf)) > 0) {
     write(STDIN_FILENO, input_buf, n);
     write(STDIN_FILENO, "\n", 1);
   }
