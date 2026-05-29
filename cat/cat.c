1b7edf10e64b2b5777af51934ab1fb37f3b2462f
khizer <mekhizer@gmail.com>
2026-05-29 14:54:36 +0100

    abandoned opening/reading directory implementation, deemed it unnecessary

--- cat/cat.c
+++ cat/cat.c
@@ -1,52 +1,65 @@
-#include <string.h>
-#include <unistd.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
 #define MAXINPUT 1024
+
 ssize_t read_input(char *input_buf);
 
-ssize_t read_input(char *input_buf) {
+/* Read input from STDIN and store it in INPUT_BUF.
+   Return the number of bytes read or -1 for error. */
+ssize_t read_input(char *input_buf)
+{
   ssize_t n = -1;
 
   // read MAXINPUT chars from stdin and store in buffer
-  if ((n = read(STDIN_FILENO, input_buf, MAXINPUT)) == -1) {
+  if ((n = read(STDIN_FILENO, input_buf, MAXINPUT)) == -1)
+  {
     perror("read failed");
     return n;
   }
 
-
   // strip trailing '\n' and null terminate buffer
-  input_buf[n] = 0;
   input_buf[strcspn(input_buf, "\n")] = 0;
   return n;
 }
 
-int file_exists(char *input_buf) {
-  /* takes input and checks if arg file eists */
-  /* if path exists with `/` its absolute */
+int read_file(char *input_buf)
+{
+  /* i don't need to directly deal with opening & reading from dirs.
+     only need to know if the file is a dir for error message */
+  int fd;
+  int absolute_path = input_buf[0] == '/';
+  char file_bytes[MAXINPUT];
 
-  /* i could just use opendir() instead of manually handling this
-   * look into that and see what i need to do. i might also just
-   * make my own version of 'opendir', ill read through what it does and see
-   * if that seems reasonable.
-   */
-  int is_absolute_path = 0;
-  int file_exists = 0;
+  if (absolute_path)
+  {
+    if ((fd = open(input_buf, O_RDONLY)) == -1)
+    {
+      perror("file opening failed");
+      return fd;
+    }
 
-  if (input_buf[0] == '/') {
-    is_absolute_path = 1;
+    // read(fd, void *buf, MAXINPUT);
   }
-
-  if (is_absolute_path) {
-
+  else
+  { // if relative path, get working dir's fd and pass into openat()
+    int working_dir_fd = open("./", O_RDONLY);
+    fd = openat(working_dir_fd, input_buf, O_RDONLY);
   }
+  return fd;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   // +1 for null termination character
-  char input_buf[MAXINPUT+1];
+  char input_buf[MAXINPUT + 1];
   ssize_t n;
 
-  while ((n = read_input(input_buf)) > 0) {
+  while ((n = read_input(input_buf)) > 0)
+  {
     write(STDIN_FILENO, input_buf, n);
     write(STDIN_FILENO, "\n", 1);
   }
